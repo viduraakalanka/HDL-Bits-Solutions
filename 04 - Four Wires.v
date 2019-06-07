f205c90a82417584abc680aa54b2706e9adc8b46
Vidura Akalanka <37825080+viduraakalanka@users.noreply.github.com>
2019-06-07 14:21:33 +0530

    Update 04 - Four Wires.v

--- 04 - Four Wires.v
+++ 04 - Four Wires.v
@@ -1,6 +1,6 @@
 module top_module( input a,b,c,output w,x,y,z );
     
-    assign w = a;
+    assign w = a; //assign statements are executed in parallel
     assign x = b;
     assign y = b;
     assign z = c;
