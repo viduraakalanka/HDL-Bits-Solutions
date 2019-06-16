3e1ca9c75d131be59a52749e8becb58f321f6f77
viduraakalanka <37825080+viduraakalanka@users.noreply.github.com>
2019-06-16 18:12:02 +0530

    Update 09 - Declaring Wires.v

--- 1 - Basics/09 - Declaring Wires.v
+++ 1 - Basics/09 - Declaring Wires.v
@@ -1,6 +1,17 @@
-module top_module( 
-    input a, 
-    input b, 
-    output out );
-    assign out = ~(a^b); 
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+	
+    wire int1,int2;
+    
+    assign int1 = a&b;
+    assign int2 = c&d;
+    assign out = int1|int2;
+    assign out_n = ~out;
+    
 endmodule
